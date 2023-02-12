40e7a2a3e2b9214baec9c91febc414f82dccc39b
Adam Krisciunas <agkrisciunas@gmail.com>
2023-02-12 14:09:41 -0500

    renamed it

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,16 +1,15 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
+public class CtoFConverter {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double celsius;
         double fahrenheit;
         boolean valid = false;
 
-        while (!valid) {
-            try {
-                System.out.print("Enter a temperature in Celsius: ");
+        do {
+            System.out.print("Enter a temperature in Celsius: ");
+            if (input.hasNextDouble()) {
                 celsius = input.nextDouble();
 
                 // Convert the temperature from Celsius to Fahrenheit
@@ -18,10 +17,11 @@ public class Main {
 
                 System.out.println("The equivalent temperature in Fahrenheit is: " + fahrenheit);
                 valid = true;
-            } catch (InputMismatchException e) {
+            } else {
                 System.out.println("Invalid input, please enter a valid temperature in Celsius.");
                 input.nextLine();
             }
-        }
+        } while (!valid);
     }
 }
+
